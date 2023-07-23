0daf4e743967aa23b248889d7582d64fb401dd06
realmadrid50 <wozals77@gmail.com>
2023-07-23 11:13:11 +0200

    s

--- src/index.js
+++ src/index.js
@@ -3,18 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './app.css';
 import './presale.css';
 import App from './App';
-import Presale from './Presale';
+// import Presale from '../../Presale';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import Routes here
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router> {/* Wrap the App component with Router */}
+      <App />
+    </Router>
   </React.StrictMode>,
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
