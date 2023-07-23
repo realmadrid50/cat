96ec7e62a379f94c4b9e4474208de54c2d8f5f75
realmadrid50 <wozals77@gmail.com>
2023-07-23 10:43:22 +0200

    s

--- src/index.js
+++ src/index.js
@@ -11,11 +11,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
+
+      <App></App>
       <Routes>
-        {/* Route for the Presale component */}
         <Route path="/presale" element={<Presale />} />
 
-        {/* Default route for the App component */}
         <Route path="/" element={<App />} />
       </Routes>
     </Router>
